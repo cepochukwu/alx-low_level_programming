6bd28c627d873a46307eb23063aa0d49ccd15965
CHINONSO_EMMANUEL_OKERE <cepovin2@gmail.com>
2022-10-13 10:58:57 -0700

    task 4-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * this program prints the description of c programming language
+ */
+int main(void)
+{	
+	puts("\"programming is like building a multilingual puzzle\"\n");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * this progran describes c
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
